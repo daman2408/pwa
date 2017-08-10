f34e42b3b7260892cc9082be8891c2d983bdaef5
Damanvir Mangat <daman@Damanvirs-MacBook-Pro.local>
2017-08-09 21:10:00 -0400

    re-aligned form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
 
 class UserForm extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div>
-            <form onSubmit={this.props.submit}>
-              <div className="form-group">
-                <label>Email:
-                  <input type="email" className="form-control" />
-                </label>
-              </div>
-              <div className="form-group">
-                <label>Password:
-                  <input type="password" className="form-control"/>
-                </label>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 offset-3">
+                        <form onSubmit={this.props.submit}>
+                            <div className="form-group row">
+                                <label className="col-2 col-form-label">Email</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="email" id="username"/>
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col-2 col-form-label">Password</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="password" id="example-text-input"/>
+                                </div>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default UserForm;
